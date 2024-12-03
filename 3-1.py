b11c05cd472c1869d9c848122ea4323d9c32b5a2
arturchichorro <arturchichorro@gmail.com>
2024-12-03 13:23:45 +0000

    Solved 3.1 using two different methods

--- 3-1.py
+++ 3-1.py
@@ -25,3 +25,20 @@ for s in matches:
 print(sum(reduce(lambda x, y: x*y, pair) for pair in n_pairs))
 
 # Solution 2 not using re
+num_pairs = []
+
+start = 0
+while (start := data.find("mul(", start)) != -1:
+    end = data.find(")", start)
+    if end != -1:
+        candidate = data[start+4:end]
+
+        if "," in candidate and all(part.strip().isdigit() for part in candidate.split(",")):
+            # print(candidate.split(","))
+            num_pairs.append([int(x) for x in candidate.split(",")])
+        
+        start = start + 4
+    else:
+        break
+
+print(sum(reduce(lambda x, y: x*y, pair) for pair in num_pairs))
