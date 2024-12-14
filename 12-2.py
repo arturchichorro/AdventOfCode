310afb58dd792d3df1cf7cbc7347aec1df8876e1
arturchichorro <arturchichorro@gmail.com>
2024-12-14 14:35:23 +0000

    solved 12-2.py

--- 12-2.py
+++ 12-2.py
@@ -1,24 +1,29 @@
-t_data = """AAAA
+# Expected 64
+t0_data = """AAAA
+AAAA
+AAAA
+AAAA"""
+
+# Expected 80
+t1_data = """AAAA
 BBCD
 BBCC
 EEEC"""
 
-t1_data = """OOOOO
-OXOXO
-OOOOO
-OXOXO
-OOOOO"""
+# Expected 236
+t2_data = """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"""
 
-t2_data = """RRRRIICCFF
-RRRRIICCCF
-VVRRRCCFFF
-VVRCCCJFFF
-VVVVCJJCFE
-VVIVCCJJEE
-VVIIICJJEE
-MIIIIIJJEE
-MIIISIJEEE
-MMMISSJEEE"""
+# Expected 368
+t3_data = """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"""
 
 data = """UUUUUUUUUUUUUUUUUUUDQQQQQQQOOOOOHHSSSSSYYYYYYYYGGGGNNNNNNNNTTTTTTTTTTTTTTDDDDDDDDDDDDDDDDDDNNNCCNJJJJOOHHHDDDDDDDDDDDDDDDDDDXXMBBBBMMCCCCCCC
 UUUKUUUUUUUUUUUUUDDDDDDDQQQOOOOOOOOOSSSSSYYYYYYYYGGGGGNNNNTTTTTTTTTTTTTTTTTTTTDDDDDDIIDDDNNNNNCCNJJJOOOHDDDDDDDDDDXDDDDDDDMMMXMMMMMMMMCCCCCC
@@ -161,30 +166,55 @@ GGGGGGGGGAAAAAAAAJJNNNNNNPPPPPPPPPPOOOOOOOOUUUGGGGGGGGGGGIIIIIIIIIIIIIIIIHHHHHHH
 GGGGGGGGAAAAAAAAAAJNNNNNNNPNPPPPXXXOOOOOOOOOUUGGGGGGGGGGGGIIIIIIIIIIIIHHIIHHHHHHHHHIIIIJJJHXXXXXXXWXFFFXXXXZZZZZZZZZZZVZZZZZZZYYYYEEEEEEHHHH
 GGGGGGGGGGGAAAJJJJJNNNNNNNNNPPPXXXXOOXXOOOOOGGGGGGGGGGGGGGGGIIIIIIIIIIIHHHHHHHHHHHHHHHHHHHHHXXXXXXXXXFFFFXXZZZZZZZZZZZZZZZZZZZZZYYEEEEEHHHHH"""
 
-def find_area_and_perimeter(matrix, x, y, visited):
+def get_corners(matrix, x, y):
+        NW, W, SW, N, S, NE, E, SE = [
+            is_same(matrix, x+dx, y+dy, matrix[x][y])
+            for dx in range(-1, 2) 
+            for dy in range(-1, 2) 
+            if dx or dy
+        ]
+        return sum([
+            N and W and not NW, 
+            N and E and not NE, 
+            S and W and not SW, 
+            S and E and not SE, 
+            not (N or W),
+            not (N or E),
+            not (S or W),
+            not (S or E)
+        ])
+    
+def is_same(matrix, x, y, type):
+    return (
+        x in range(len(matrix)) and 
+        y in range(len(matrix[0])) and 
+        matrix[x][y] == type)
+
+def find_area_and_corners(matrix, x, y, visited):
     type = matrix[x][y]
     stack = [(x, y)]
     a = 0
-    p = 0
+    c = 0
 
     visited.add((x,y))
 
     while stack:
         x, y = stack.pop()
         a += 1
+        c += get_corners(matrix, x, y)
 
         for dx, dy in [(0,1), (0,-1), (1,0), (-1,0)]:
             nx, ny = x + dx, y + dy
 
             if not (0 <= nx < len(matrix) and 0 <= ny < len(matrix[0])):
-                p += 1
+                continue
             elif matrix[nx][ny] != type:
-                p += 1
+                continue
             elif (nx, ny) not in visited:
                 visited.add((nx, ny))
                 stack.append((nx,ny))
     
-    return a, p
+    return a, c
 
 def calc_cost(matrix):
     visited = set()
@@ -193,10 +223,10 @@ def calc_cost(matrix):
     for x in range(len(matrix)):
         for y in range(len(matrix[0])):
             if (x, y) not in visited:
-                a, p = find_area_and_perimeter(matrix, x, y, visited)
-                result += a * p
+                a, s = find_area_and_corners(matrix, x, y, visited)
+                result += a * s
 
     return result
 
-matrix = [list(line) for line in t_data.strip().split("\n")]
+matrix = [list(line) for line in data.strip().split("\n")]
 print(calc_cost(matrix))
