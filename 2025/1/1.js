61273b486b01e2df80a66d6e5228544d5b8fb441
arturchichorro <arturchichorro@gmail.com>
2025-12-01 20:44:32 +0100

    removed js files

--- 2025/1/1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var data = fs.readFileSync("input.txt", "utf8");
-var lines = data.split(/\r?\n/);
-for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
-    var line = lines_1[_i];
-    console.log(line);
-}
