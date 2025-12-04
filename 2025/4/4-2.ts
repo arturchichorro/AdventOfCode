3941d4f1e34b24b603c96d736a1432dacb1a9ddd
arturchichorro <arturchichorro@gmail.com>
2025-12-04 23:59:20 +0100

    solved 4-2

---
+++ 2025/4/4-2.ts
@@ -0,0 +1,42 @@
+import * as fs from "fs";
+
+const data = fs.readFileSync("input.txt", "utf8");
+const lines = data.split(/\r?\n/).map((l) => l.split(""));
+
+const DIRECTIONS = [
+    [0,1], [1,0], [1,1], [0, -1], 
+    [-1, 0], [-1, -1], [1, -1], [-1, 1]
+];
+const rows = lines.length;
+const cols = lines[0].length;
+
+const inBounds = (row: number, col: number) => row >= 0 && row < rows && col >= 0 && col < cols;
+
+const check = (row: number, col: number, matrix: String[][]) => {
+    if (matrix[row][col] !== "@") return false;
+    let count = 0
+    for (const [a, b] of DIRECTIONS) {
+        if (!inBounds(row + a, col + b)) continue;
+        if (matrix[row + a][col + b] === '@') count ++;
+        if (count > 3) return false;
+    }
+    return true;
+}
+
+let res=0;
+while (true) {
+    let found = false;
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (check(r, c, lines)) {
+                lines[r][c] = "."
+                res++;
+                found = true;
+            };
+        }   
+    }
+    if (!found) break;
+}
+
+console.log(res);
