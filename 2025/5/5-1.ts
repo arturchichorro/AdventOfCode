35f5d3d0a618a2490fbbe8dc33bdbe14f632d53d
arturchichorro <arturchichorro@gmail.com>
2025-12-05 09:05:47 +0100

    solved 5-1

---
+++ 2025/5/5-1.ts
@@ -0,0 +1,19 @@
+import * as fs from "fs";
+
+const data = fs.readFileSync("input.txt", "utf8");
+const firstSplit = data.split(/\r?\n\n/);
+const ranges = firstSplit[0].split(/\r?\n/);
+const ingredients = firstSplit[1].split(/\r?\n/);
+
+const rangesParsed = ranges.map(r => r.split("-").map(Number));
+
+let res = 0;
+for (const ing of ingredients) {
+    for (const range of rangesParsed) {
+        if (Number(ing) >= range[0] && Number(ing) <= range[1]) {
+            res += 1;
+            break;
+        } 
+    }
+}
+console.log(res);
